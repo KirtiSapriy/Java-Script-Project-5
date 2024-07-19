5d037ecb9dd8e5147f4a02e4ca1e5ff5ddbfd924
KirtiSapriy <sapriyak01@gmail.com>
2024-07-19 18:30:01 +0530

    Projet Task

---
+++ 3.js
@@ -0,0 +1,16 @@
+let arr = ["apple", "banana", "cherry"];
+let ans = [];
+
+
+for (let a = 0; a < arr.length; a++) {
+
+        if (arr[a].length > 5) {
+                ans.push(arr[a])
+        }
+
+
+
+}
+
+
+console.log(ans);
---
+++ 5.js
@@ -0,0 +1,10 @@
+let str = "Hello";
+
+
+
+for (let a = 1; a <= str.length; a++) {
+        if (a == 3) {
+                console.log(str[a]);
+        }
+}
+
